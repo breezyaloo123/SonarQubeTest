e314e6eb6df55515cdf5c1fc60e4ad2d18197947
breezyaloo123 <alassanesall771@gmail.com>
2021-12-07 16:55:45 +0000

    test class

--- src/test/java/com/example/apiroboframework/ApiRoboFrameworkApplicationTests.java
+++ src/test/java/com/example/apiroboframework/ApiRoboFrameworkApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 //import java.util.List;
 
-@SpringBootTest
+@SpringBootTest(classes = ApiRoboFrameworkApplication.class)
 class ApiRoboFrameworkApplicationTests {
 
     @Test

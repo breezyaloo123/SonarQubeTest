11323ef5e9e94b62296b30e5af4be8c9088ba913
breezyaloo123 <alassanesall771@gmail.com>
2021-12-07 15:56:02 +0000

    secnd test

--- src/test/java/com/example/apiroboframework/ApiRoboFrameworkApplicationTests.java
+++ src/test/java/com/example/apiroboframework/ApiRoboFrameworkApplicationTests.java
@@ -7,10 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+//import java.util.List;
 
 @SpringBootTest
 class ApiRoboFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
 
 
 //    @Autowired

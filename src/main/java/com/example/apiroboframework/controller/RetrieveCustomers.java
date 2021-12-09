aa4b91607bf81dd08a6c966d5e83205348aa14ca
breezyaloo123 <alassanesall771@gmail.com>
2021-12-09 10:37:07 +0000

    sonar conf

--- src/main/java/com/example/apiroboframework/controller/RetrieveCustomers.java
+++ src/main/java/com/example/apiroboframework/controller/RetrieveCustomers.java
@@ -21,7 +21,7 @@ public class RetrieveCustomers {
 }
 @GetMapping(path = "/customers/{id}")
     public String retrieveCustomer(@PathVariable Integer id){
-    //String src = "";
+    String src = "";
     if(customerRepository.existsById(id)){
     return customerRepository.findById(id).isPresent()
             ? customerRepository.getById(id).getFirstname()
